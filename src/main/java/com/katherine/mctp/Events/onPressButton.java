aec6cf5ee901dc5f524f29004c94ce95c286d4e4
Katherine <hurstkatherine@hotmail.co.uk>
2021-04-26 22:56:43 +0100

    Made it so you're teleported to the  next stage if you get a correct answer

--- src/main/java/com/katherine/mctp/Events/onPressButton.java
+++ src/main/java/com/katherine/mctp/Events/onPressButton.java
@@ -29,27 +29,43 @@ public class onPressButton implements Listener {
         Location b_under_loc = block.getLocation().add(new Location(0,-1,0));
         Level level = b_loc.level;
         Block block_under = level.getBlock(b_under_loc);
-        String answer = null;
+        char answer = 'Z';
         String wellDone = config.getString("wellDone");
 
+        ///Question 1
+        Location nWCorner = new Location(17,4,135);
+        Location sECorner = new Location(24, 4, 145);
+        Location correctTp = new Location (20, 4 ,160);
+        Location incorrectTp = new  Location(18,4,136);
+        char correctAnswer = 'A';
 
         if (action.equals(PlayerInteractEvent.Action.RIGHT_CLICK_BLOCK)){
             if (block.getName().equals("Stone Button")) {
                 //player.sendMessage("The block under this one is: " + block_under.getName());
+                if ((player.getLocation().getX()>=nWCorner.getX())&&(player.getLocation().getX()<=sECorner.getX())&&(player.getLocation().getZ()>=nWCorner.getZ())&&(player.getLocation().getZ()<=sECorner.getZ())){
+                    if (block_under.getName().equals("Red Wool")) {
+                        answer = 'A';
+                    } else if (block_under.getName().equals("Orange Wool")) {
+                        answer = 'B';
+                    } else if (block_under.getName().equals("Yellow Wool")) {
+                        answer = 'C';
+                    }
 
-                if (block_under.getName().equals("Red Wool")) {
-                    answer = "A";
-                } else if (block_under.getName().equals("Orange Wool")) {
-                    answer = "B";
-                } else if (block_under.getName().equals("Yellow Wool")) {
-                    answer = "C";
+                    player.sendMessage(TextFormat.GREEN + "Your answer is " + answer);
+                    //check if you're in the right place
+                    if (answer == correctAnswer) {
+                        player.sendMessage("Well done! You've got to the next stage");
+                        player.teleport(correctTp);
+                    } else {
+                        player.teleport(incorrectTp);
+                        player.sendMessage("Not quite, have another go!");
+                    }
+            }else{
+                    player.sendMessage("You're not in the right place");
                 }
 
-                player.sendMessage(TextFormat.GREEN + "Your answer is " + answer);
-                player.sendMessage(wellDone);
-
-
             }
+
 
 
         }
